03c9d5b6dcf44e06613e67a22a2f55b868d033b8
gill1026 <gill1026@hanyang.ac.kr>
2026-08-20 05:26:20 +0000

    남은 디버깅 도구와 Humble Jazzy 호환 수정 반영

--- orda/perception/object_detection/src/object_detection.cpp
+++ orda/perception/object_detection/src/object_detection.cpp
@@ -68,7 +68,13 @@
 #include <std_msgs/msg/int32.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
-#include <cv_bridge/cv_bridge.hpp>
+#if __has_include(<cv_bridge/cv_bridge.hpp>)
+#include <cv_bridge/cv_bridge.hpp>  // Jazzy
+#elif __has_include(<cv_bridge/cv_bridge.h>)
+#include <cv_bridge/cv_bridge.h>    // Humble
+#else
+#error "cv_bridge header not found"
+#endif
 #include <ament_index_cpp/get_package_prefix.hpp>
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <fstream>
